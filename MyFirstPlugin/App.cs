d27c2d6ab7bf077f4bc69750a0458d0efede16cf
Никита <@DESKTOP-TM77RPK>
2024-01-15 11:05:28 +0100

    Add project files.

---
+++ MyFirstPlugin/App.cs
@@ -0,0 +1,21 @@
+﻿using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstPlugin
+{
+    public class App : IExternalApplication
+    {
+        public Result OnStartup(UIControlledApplication a)
+        {
+
+        }
+        public Result OnStartup(UIControlledApplication a)
+        {
+
+        }
+    }
+}
